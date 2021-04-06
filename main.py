bb12389683594e6cb1c0221de099e998a0d2152b
haris877 <81170462+haris877@users.noreply.github.com>
2021-04-06 14:32:02 +0200

    senaste backend
    senaste backend

--- main.py
+++ main.py
@@ -11,43 +11,24 @@
 # @Author: Erik Johnsson
 #=====================================
 
-import numpy as np 
-import pandas as pd
-import matplotlib as plt
-from sklearn.ensemble import RandomForestRegressor
-
-regressor = RandomForestRegressor(n_estimators = 12, random_state = 0)
-
-def train_model():
-    progs = pd.DataFrame(
-        [
-        ['Action',1,673.50],
-        ['Sports',2,324.80],
-        ['Shooter',3,462.80],
-        ['Roleplaying',4,305.72],
-        ['Platform',5,120.69],
-        ['Misc',6,238.05],
-        ['Racing',7,123.69],
-        ['Fighting',8,82.08],
-        ['Simulation',9,71.04],
-        ['Puzzle',10,21.24],
-        ['Adventure',11,61.05],
-        ['Strategy',12,35.90]
-    ],
-    index=["Action", "Sports", "Shooter", "Role-Playing", "Platform", "Misc", "Racing", "Fighting", "Simulation", "Puzzle", "Adventure", "Strategy"],
-    columns=['Genre','Level','GLSales'])
-
-    X = progs.iloc[:, 1:-1].values
-    y = progs.iloc[:, -1].values
-
-    regressor.fit(X, y)
-
-def predict(Level):
-
-    prediction = regressor.predict([[Level]])
-
-    print(prediction)
-
-# kalla på båda funktionerna train_model() och predict():
+from sanic import Sanic, response
+from randomforest import train_model, predict
+from sanic.exceptions import NotFound
+app = Sanic(__name__)
 train_model()
-predict(Level=6) # level 6 = Misc
+
+@app.post('/api/predict')
+async def predict_results(req):
+  values = req.json
+  prediction = predict(values['Level'])
+  return response.json(prediction)
+
+app.static('/', './dist')
+
+@app.exception(NotFound)
+async def ignore_404s():
+  return await response.file('./dist/index.html')
+
+if __name__ == "__main__":
+  app.run(port=5500)  
+
---
+++ randomforest.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+import sqlite3
+
+regressor = RandomForestRegressor(n_estimators = 12, random_state = 0)
+
+def train_model():
+  con = sqlite3.connect('dataset.db')
+  dataset = pd.read_sql_query('SELECT * FROM dataset', con)
+  
+  X = dataset.iloc[:, 1:-1].values
+  y = dataset.iloc[:, -1].values
+
+  regressor.fit(X, y)
+
+
+def predict(Level):
+  prediction = regressor.predict([[Level]])
+
+  print(prediction)
+
+# kalla på båda funktionerna train_model() och predict():
+train_model()
+predict(Level)  

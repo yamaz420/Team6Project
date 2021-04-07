faff34fa4605c4cf8f985d6a96d27962fcfad80b
Erik Torres Puente <81220576+ErikTP@users.noreply.github.com>
2021-04-07 12:55:32 +0200

    Genre Category HTML Update

--- dist/main.js
+++ dist/main.js
@@ -43,7 +43,6 @@ predictButton.addEventListener("click", ()=>{
      });
  })
  
-     
  var ctx = document.getElementById('canvas').getContext('2d');
  var myChart = new Chart(ctx, {
      type: 'line',
